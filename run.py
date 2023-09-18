89e09149eeea262d0376dcf1e8af14cd74a9e5ff
sari-rahul <>
2023-09-18 09:33:53 +0000

    Add intoduction menu and main function

--- run.py
+++ run.py
@@ -1,3 +1,32 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+from pprint import pprint
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('PeoplesBank')
+
+PersonalDetails = SHEET.worksheet("PersonalDetails")
+
+
+def main():
+    print("\n\n    WELCOME TO PEOPLES BANK      ")
+    print("----------------------------------")
+    
+    print("\n\nWHICH OF THE FOLLOWING SERVICES DO YOU WANT TO ACCESS ??\n\n")
+    print("1.Open New Account\n"
+          "2.Deposit Amount\n"
+          "3.Withdraw Amount\n"
+          "4.Balance Enquiry\n"
+          "5.Show Customer Details\n"
+          "6.Close an Account")
+
+          
+main()
