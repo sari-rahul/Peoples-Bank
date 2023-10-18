60c6dbe4b787d4f438cf9339a94f3e27ca06ec96
sari-rahul <>
2023-10-18 15:02:50 +0000

    Edit admin_login()

--- run.py
+++ run.py
@@ -583,7 +583,7 @@ def view_acc_holders(username, pin):
         else:
             print("Press'0' to go back")
 
-
+    
 def admin_login(username, pin):
     """
     Opens a panel for admin to view all users, delete a user and to logout.
@@ -624,39 +624,15 @@ def admin_login(username, pin):
                 admin_login(username, pin)
         # View the account of user
         elif admin_choice == "3":
-            clear()
-            print("\nEnter the username for the account you want to see.")
-            account_un = input(">>").capitalize()
-            # Searches the username in Database
-            get_name = PersonalDetails.find(account_un, in_column=1)
-            if get_name:
-                clear()
-                print(heading_art.logo)
-                print("\nLOADING DATA.....\n\n")
-                sleep(2)
-                # Finds the user's individual worksheet.
-                full_data = SHEET.worksheet(account_un)
-                full_data_arr = full_data.get_all_values()
-                acc_holder_data = full_data_arr[1:]
-                print(tabulate(acc_holder_data, headers=['Date', 'Deposit', 'Withdraw', 'Balance', 'PIN satus']))
-                answer_loop = True
-                while answer_loop:
-                    answer = input("\n\nType '0' to go back\n")
-                    if answer == "0":
-                        admin_login(username, pin)
-                        answer_loop = False
-                    else:
-                        input("\n\nType '0' to go back\n")
-
-            else:
-                print("\nUser not found")
-                break
-
+            admin_user_acc(username, pin)
+            break    
+        # logging out for admin
         elif admin_choice == "4":
             logging_out()
             sleep(2)
             welcome()
             break
+        # go back to admin panel
         elif admin_choice == "0":
             admin_login(username, pin)
             sleep(2)
